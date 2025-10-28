b4a75cf12a4462f0a93b70b97c3087766cc572c2
Vineeth MN <vineethmn51@gmail.com>
2025-10-28 17:05:25 +0000

    staging and production files addede

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Backend API is working!");
+  res.send("Backend API is working at root!");
+});
+
+app.get("/api", (req, res) => {
+  res.send("Backend API is working from /api!");
 });
 
 app.listen(3000, () => console.log("Backend running on port 3000"));
